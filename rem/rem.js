c30f2d70fe9a8e713cf5bca10b127b03983339de
Jennifer <879905159@qq.com>
2016-08-16 18:27:37 +0800

    设置html的font-size,用于rem

---
+++ rem/rem.js
@@ -0,0 +1,19 @@
+(function() {
+    function rem (){
+        var defaults = {
+            maxS: 750, //设计稿宽度
+            minS: 320, //最小屏幕宽度
+            maxFs: 100, //最大宽度字体大小
+        }
+        defaults.minFs = defaults.maxFs * defaults.minS / defaults.maxFs; //最小宽度时的字体大小
+        // 按屏幕宽度设置字体大小
+        var fs = window.screen.width / defaults.maxS * defaults.maxFs;
+
+        // 处理边界问题
+        fs = fs > defaults.maxFs ? defaults.maxFs : (fs < defaults.minFs ? defaults.minFs : fs);
+        if(document.getElementsByTagName){
+        	document.getElementsByTagName("html")[0].style.fontSize=fs+"px";
+        }
+    }
+    rem();
+})(window, undefined);
